30ab863c0c0df8afb60ee75b8e9da0344c28edd5
vivekananda-reddy <vivekananda-reddy@missionrnd.com>
2016-03-07 19:52:57 +0530

    Submit Commit

--- src/NumbertoStr.cpp
+++ src/NumbertoStr.cpp
@@ -21,5 +21,61 @@ NOTES: Don't create new string.
 
 
 void number_to_str(float number, char *str,int afterdecimal){
+	float num=number;
+	int n,i,j=0,temp,k,negative=0;
+	char c;
+	for (i = 1; i <= afterdecimal; i++)
+	{
+		num = num * 10;
+	}
+	n = (int)num;
+	if (n < 0)
+	{
+		n = n*-1;
+		negative = 1;
+	}
+	while (n != 0)
+	{
+		
+		temp = n % 10;
+		str[j] = temp + '0';
+		n = n / 10;
+		j++;
+		if (j == afterdecimal)
+		{
+			str[j] ='.';
+			j++;
+		}
+	}
+	while (j <= (afterdecimal+1)&&afterdecimal!=0)
+	{
+		if (j<afterdecimal||j>afterdecimal)
+		{
+			str[j] = '0';
+
+		}
+		
+		if (j == afterdecimal)
+		{
+			str[j] = '.';
+			
+		}
+		j++;
+	}
+	if (negative == 1)
+	{
+		str[j] = '-';
+		j++;
+	}
+	str[j] = '\0';
+
+	for (i = 0, k = j - 1; i < k; i++, k--)
+	{
+		c = str[i];
+		str[i] = str[k];
+		str[k] = c;
+	}
+
 	
+
 }
