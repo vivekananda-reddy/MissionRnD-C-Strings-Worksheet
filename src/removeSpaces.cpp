153951941e270b72670afda04b8dc56b43ce8e13
vivekananda-reddy <vivekananda-reddy@missionrnd.com>
2016-03-08 19:51:35 +0530

    Submit Commit

--- src/removeSpaces.cpp
+++ src/removeSpaces.cpp
@@ -10,7 +10,33 @@ ERROR CASES: Return '\0' for invalid inputs.
 
 NOTES: Don't create new string.
 */
-
+#include<stddef.h>
+void deletespaces(char*, int);
 char removeSpaces(char *str) {
-	return '\0';
+	int i, j;
+	if (str == NULL || str[0] == '\0')
+		return '\0';
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+		{
+			deletespaces(str, i);
+			break;
+		}
+	}
+}
+
+void deletespaces(char* a, int j)
+{
+	int i=j+1;
+	while (a[i] != '\0')
+	{
+		if (a[i] != ' ')
+		{
+			a[j] = a[i];
+			j++;
+		}
+		i++;
+	}
+	a[j] = '\0';
 }
